b1286bfd5dcb96da7dfccd6c0ebeb40c4b438eff
vanotis720 <vanotis720@gmail.com>
2024-08-02 13:27:05 +0200

    remove inputs components

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Text, View } from 'react-native';
 
 export default WelcomeScreen = () => {
 
+    return (
+        <View>
+            <Text>Hello from earth</Text>
+        </View>
+    );
 }
